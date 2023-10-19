32a5848dd4442e2d32b35e475f638caba877fab4
Frank_Alva <u202020026@upc.edu.pe>
2023-10-19 10:40:54 -0500

    feat(notification): created NotificationRepository

--- src/main/java/com/deviot/agripurebackend/notification/domain/entities/Notification.java
+++ src/main/java/com/deviot/agripurebackend/notification/domain/entities/Notification.java
@@ -4,22 +4,25 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import jakarta.persistence.*;
+
+import java.util.Date;
+
 @Getter
 @Setter
 @Data
 
-public class notification {
+public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int userId;
-    private String createAt;
+    private Date createAt;
     private String title;
     private String body;
     private String importance;
 
-    public notification(Long id, int u, String c, String t, String b, String ii){
+    public Notification(Long id, int u, Date c, String t, String b, String ii){
         this.id=id;
         this.userId=u;
         this.createAt=c;

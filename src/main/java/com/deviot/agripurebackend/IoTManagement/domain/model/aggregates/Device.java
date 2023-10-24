bde540996209e6138a4d3fde9f3ce9a9fa350ce0
FranchescoSoto <u201821078@upc.edu.pe>
2023-10-24 12:53:11 -0500

    Create base directories and files

---
+++ src/main/java/com/deviot/agripurebackend/IoTManagement/domain/model/aggregates/Device.java
@@ -0,0 +1,4 @@
+package com.deviot.agripurebackend.IoTManagement.domain.model.aggregates;
+
+public class Device {
+}
---
+++ src/main/java/com/deviot/agripurebackend/IoTManagement/domain/model/commands/AssignDeviceToCropCommand.java
@@ -0,0 +1,4 @@
+package com.deviot.agripurebackend.IoTManagement.domain.model.commands;
+//upc-pre-202302-si729-sandbox
+public record AssignDeviceToCropCommand() {
+}

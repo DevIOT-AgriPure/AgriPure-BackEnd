5170ab9d5b7939d692f80cf8c7d311bf9d097a2c
Frank_Alva <u202020026@upc.edu.pe>
2023-10-24 12:42:30 -0500

    feat(notification): refactor all packages and modify the structure

---
+++ src/main/java/com/deviot/agripurebackend/notification/domain/model/aggregates/Notification.java
@@ -0,0 +1,35 @@
+package com.deviot.agripurebackend.notification.domain.model.aggregates;
+
+import com.deviot.agripurebackend.notification.domain.model.enums.NotificationImportance;
+import lombok.*;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Data
+@Builder
+@Entity
+@Table(name = "notification")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "created_at")
+    private Date createAt;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "body")
+    private String body;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "importance")
+    private NotificationImportance importance;
+
+}
+
--- src/main/java/com/deviot/agripurebackend/notification/services/NotificationService.java
+++
@@ -1,4 +0,0 @@
-package com.deviot.agripurebackend.notification.services;
-
-public class NotificationService {
-}

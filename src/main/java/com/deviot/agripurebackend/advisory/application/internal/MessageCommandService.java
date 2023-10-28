61741348dc33b1b582bc78548063af77da0ed549
DiegoTalledoS18 <diegotasa35@hotmail.com>
2023-10-28 10:47:53 -0500

    feat(Advisory-management): Message repository created

---
+++ src/main/java/com/deviot/agripurebackend/advisory/application/internal/MessageCommandService.java
@@ -0,0 +1,11 @@
+package com.deviot.agripurebackend.advisory.application.internal;
+
+import com.deviot.agripurebackend.advisory.domain.Services.IMessageCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MessageCommandService implements IMessageCommandService {
+    
+}

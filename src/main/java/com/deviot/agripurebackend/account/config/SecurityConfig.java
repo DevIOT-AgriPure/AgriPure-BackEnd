6d64e4e4bbb5baf84aa32fa7b959c0041b5a097c
FranchescoSoto <u201821078@upc.edu.pe>
2023-11-01 20:50:54 -0500

    feat: add cors configuration for profiles

--- src/main/java/com/deviot/agripurebackend/account/config/SecurityConfig.java
+++ src/main/java/com/deviot/agripurebackend/account/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                                 ,"/swagger-ui.html"
                                 ,"/api/v1/profiles/**")
                                 .permitAll()
-                                .anyRequest().authenticated()
+                                .anyRequest().authenticated()//sdre
                         )
                 .sessionManagement(
                         sessionManager->sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS ))
